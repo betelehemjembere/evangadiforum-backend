2ea9836e94c88b96b04af8a2a7d2a83536e90012
betelehemjembere <betelehemjembere@gmail.com>
2024-10-24 17:23:48 +0300

    change the host

--- Database/dbConfig.js
+++ Database/dbConfig.js
@@ -3,8 +3,8 @@ const mysql2 = require('mysql2/promise'); // Use promise-based version
 // Database connection
 const dbConnection = mysql2.createPool({
   user: process.env.USER,
-  database:process.env.DATABASE ,
-  host: "localhost",
+  database:process.env.DATABASE,
+  host: process.env.HOST,
   password: process.env.PASSWORD,
   connectionLimit: 10
 });
